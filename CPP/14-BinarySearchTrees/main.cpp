b09aff2143895a616067b0bd0e2f5a712fb73253
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-09 11:36:48 -0700

    bst

---
+++ CPP/14-BinarySearchTrees/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public: 
+        int value;
+        Node* left;
+        Node* right;
+
+        Node(int value) {
+            this->value = value;
+            left = nullptr;
+            right = nullptr;
+        }
+};
+
+class BinarySearchTree {
+    public:
+        Node* root;
+
+    public:
+        BinarySearchTree() {
+            root = nullptr;
+        }
+};
+
+
+
+
+int main() {
+    BinarySearchTree* myBST = new BinarySearchTree();
+    cout << "Root: " << myBST->root;
+
+    return 0;
+}
