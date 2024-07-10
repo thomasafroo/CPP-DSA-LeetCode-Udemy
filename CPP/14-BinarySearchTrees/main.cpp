6a0b0fea5c736105a1b2722fbc1429a92403d8bc
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-10 14:09:28 -0700

    BST insertion

--- CPP/14-BinarySearchTrees/main.cpp
+++ CPP/14-BinarySearchTrees/main.cpp
@@ -22,13 +22,48 @@ class BinarySearchTree {
         BinarySearchTree() {
             root = nullptr;
         }
+
+        bool insert(int value) {
+            Node* newNode = new Node(value);
+            if (root == nullptr) {
+                root = newNode;
+                return true;
+            }
+            Node* temp = root;
+            while (true) {
+                if (newNode->value == temp->value) return false;
+                if (newNode->value < temp->value) { // if the new node is less than the temp node
+                    if (temp->left == nullptr) {
+                        temp->left = newNode;
+                        return true;
+                    }
+                temp = temp->left;
+            } else {
+                if (temp->right == nullptr) {
+                    temp->right = newNode;
+                    return true;
+                }
+                temp = temp->right;
+                }
+            }
+        }
 };
 
 
 
 int main() {
     BinarySearchTree* myBST = new BinarySearchTree();
-    cout << "Root: " << myBST->root;
+
+    myBST->insert(47);
+    myBST->insert(21);
+    myBST->insert(76);
+    myBST->insert(18);
+    myBST->insert(52);
+    myBST->insert(82);
+
+    myBST->insert(27);
+
+    cout << myBST->root->left->right->value << endl;
 
     return 0;
 }
