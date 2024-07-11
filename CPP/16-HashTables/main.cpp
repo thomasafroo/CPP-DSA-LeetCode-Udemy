8f4537ec6224c7a27cdca953912eb068ab66c99f
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-11 15:35:04 -0700

    Hash Tables 2

--- CPP/16-HashTables/main.cpp
+++ CPP/16-HashTables/main.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
 using namespace std;
 
+class Node {
+    public: 
+        int value;
+        Node* left;
+        Node* right;
+
+        Node(int value) {
+            this->value = value;
+            left = nullptr;
+            right = nullptr;
+        }
+};
+
+class HashTable {
+    private:
+        static const int SIZE = 7;
+        // static means store one 7 in memory
+        Node* dataMap[SIZE];
+
+    public: 
+};
