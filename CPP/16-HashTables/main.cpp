7667138ec492b297d70205a8455a5148841c8cf3
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-12 11:59:13 -0700

    Node class update

--- CPP/16-HashTables/main.cpp
+++ CPP/16-HashTables/main.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
 class Node {
     public: 
+        string key;
         int value;
-        Node* left;
-        Node* right;
+        Node* next;
 
-        Node(int value) {
+        Node(string key, int value) {
+            this->key = key;
             this->value = value;
-            left = nullptr;
-            right = nullptr;
+            next = nullptr;
+
         }
 };
 
@@ -19,7 +20,20 @@ class HashTable {
         static const int SIZE = 7;
         // static means store one 7 in memory
         Node* dataMap[SIZE];
+
     public: 
+        void printTable() {
+            for (int i = 0; i < SIZE; i++) {
+                cout << i << ":" << endl;
+                if (dataMap[i]) {
+                    Node* temp = dataMap[i];
+                    while (temp) {
+                        cout << "  {" << temp->key << ", " << temp->value << "}" << endl;
+                        temp = temp->next;
+                    }
+                }
+            }
+        }
 };
 
 
