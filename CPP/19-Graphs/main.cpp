eb73f7f2c5eaed9c516958a4c35fd36c11ea8dc6
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-17 11:34:49 -0700

    private field

--- CPP/19-Graphs/main.cpp
+++ CPP/19-Graphs/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 class Graph {
-
+    private:
+        unordered_map<string, unordered_set<string> > adjList;
 
 
 };
