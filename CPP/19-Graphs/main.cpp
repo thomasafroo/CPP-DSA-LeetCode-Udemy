ad4daffc25bc20355d53b8ea8ca19a9eb2b24d99
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-18 11:13:00 -0700

    minor changes

--- CPP/19-Graphs/main.cpp
+++ CPP/19-Graphs/main.cpp
@@ -28,10 +28,14 @@ class Graph {
         }
 
         bool addEdge(string vertex1, string vertex2) {
-            adjList.at(vertex1).insert(vertex2);
-            adjList.at(vertex2).insert(vertex1);
+            if (adjList.count(vertex1) != 0 && adjList.count(vertex2) != 0) {
+                adjList.at(vertex1).insert(vertex2);
+                adjList.at(vertex2).insert(vertex1);
+                return true;
+            }
+            return false;
         }
-        
+
 
 };
 
@@ -39,7 +43,8 @@ int main() {
     Graph* myGraph = new Graph();
 
     myGraph->addVertex("A");
-
+    myGraph->addVertex("B");
+    
     myGraph->printGraph();
 
 
