cff25aa9f076444a2ce4828a4cf7a75b7cf729d1
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-18 12:22:28 -0700

    removeEdge

--- CPP/19-Graphs/main.cpp
+++ CPP/19-Graphs/main.cpp
@@ -37,7 +37,10 @@ class Graph {
         }
 
         bool removeEdge(string vertex1, string vertex2) {
-            
+            if (adjList.count(vertex1) != 0 && adjList.count(vertex2) != 0) {
+                adjList.at(vertex1).erase(vertex2);
+                adjList.at(vertex2).erase(vertex1);
+            }
 
 
         }
