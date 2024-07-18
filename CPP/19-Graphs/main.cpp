2eeba5b26dd4d1c2099490f12828953a2f554ea2
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-18 15:53:02 -0700

    removeVertex and main

--- CPP/19-Graphs/main.cpp
+++ CPP/19-Graphs/main.cpp
@@ -49,6 +49,17 @@ class Graph {
             return false;
         }
 
+        bool removeVertex(string vertex) {
+            if (adjList.count(vertex) == 0) return false;
+            for (auto otherVertex : adjList.at(vertex)) {
+                adjList.at(otherVertex).erase(vertex);
+
+            }
+            adjList.erase(vertex);
+            return true;
+        }
+
+
 };
 
 
@@ -58,10 +69,13 @@ int main() {
     myGraph->addVertex("A");
     myGraph->addVertex("B");
     myGraph->addVertex("C");
+    myGraph->addVertex("D");
 
     myGraph->addEdge("A", "B");
     myGraph->addEdge("A", "C");
-    myGraph->addEdge("B", "C");
+    myGraph->addEdge("A", "D");
+    myGraph->addEdge("B", "D");
+    myGraph->addEdge("C", "D");
 
     myGraph->printGraph();
 
