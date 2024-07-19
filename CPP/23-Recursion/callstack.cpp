3cd763d017c28fcf4edc5cd86425e413a0d8cacf
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-19 14:05:31 -0700

    call stack

---
+++ CPP/23-Recursion/callstack.cpp
@@ -0,0 +1,24 @@
+// call stack
+#include <iostream>
+
+using namespace std;
+
+void funcThree() {
+    cout << "Three" << endl;
+}
+
+void funcTwo() {
+    funcThree();
+    cout << "Two" << endl;
+}
+
+void funcOne() {
+    funcTwo();
+    cout << "One" << endl;
+}
+
+int main() {
+
+    funcOne();
+    
+}
