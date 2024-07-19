3399e9b4abc41a69ddd9d76f34f8b7a5e01277db
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-19 11:12:44 -0700

    left right child, parent, and swap

--- CPP/21-Heaps/main.cpp
+++ CPP/21-Heaps/main.cpp
@@ -6,11 +6,23 @@ class Heap {
     private:
         vector<int> heap;
 
-        int leftChild(int index) {
-            return 2 * index;
+        int leftChild(int index) { // 0-indexed heap
+            return 2 * index + 1;
         }
-        
 
+        int rightChild(int index) {
+            return 2 * index + 2;
+        }
+
+        int parent(int index) {
+            return (index - 1) / 2;
+        }
+
+        void swap(int index1, int index2) {
+            int temp = heap[index1];
+            heap[index1] = heap[index2];
+            heap[index2] = temp;
+        }
 
 };
 
