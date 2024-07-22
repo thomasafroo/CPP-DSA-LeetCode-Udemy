6c41c0389357154c7fb1b108f18753f16b465f52
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-21 17:15:21 -0700

    rContains

--- CPP/24-RecursiveBinarySearchTrees/main.cpp
+++ CPP/24-RecursiveBinarySearchTrees/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+
+bool rContains(Node* currentNode, int value) {
+
+}
+
+bool rContains(int value) {
+    return rContains(root, value);
+}
+
+
 int main() {
 
     return 0;
