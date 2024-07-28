bed47d70947d23dc4d984de23eb7152eb2e9ed1c
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-27 17:45:32 -0700

    main

--- CPP/26-TreeTraversal/main.cpp
+++ CPP/26-TreeTraversal/main.cpp
@@ -90,6 +90,16 @@ class BinarySearchTree {
 
 
 int main() {
+    BinarySearchTree* myBST = new BinarySearchTree();
 
+    myBST->insert(47);
+    myBST->insert(21);
+    myBST->insert(76);
+    myBST->insert(18);
+    myBST->insert(27);
+    myBST->insert(52);
+    myBST->insert(82);
+
+    myBST->BFS();
     return 0;
 }
