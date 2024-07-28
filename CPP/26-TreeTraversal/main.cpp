0dfec306ea864e14e06c0c5daf50cc19de6f53ed
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-27 17:42:10 -0700

    BST class

--- CPP/26-TreeTraversal/main.cpp
+++ CPP/26-TreeTraversal/main.cpp
@@ -17,6 +17,57 @@ class Node {
         }
 };
 
+class BinarySearchTree {
+    public:
+        Node* root;
+
+    public:
+        BinarySearchTree() {
+            root = nullptr;
+        }
+
+        bool insert(int value) {
+            Node* newNode = new Node(value);
+            if (root == nullptr) {
+                root = newNode;
+                return true;
+            }
+            Node* temp = root;
+            while (true) {
+                if (newNode->value == temp->value) return false;
+                if (newNode->value < temp->value) { // if the new node is less than the temp node
+                    if (temp->left == nullptr) {
+                        temp->left = newNode;
+                        return true;
+                    }
+                temp = temp->left;
+            } else {
+                if (temp->right == nullptr) {
+                    temp->right = newNode;
+                    return true;
+                }
+                temp = temp->right;
+                }
+            }
+        }
+
+        bool contains(int value) {
+            if (root == nullptr) return false;
+            Node* temp = root;
+            while (temp) {
+                if (value < temp->value) {
+                    temp = temp->left;
+                } else if (value > temp->value) {
+                    temp = temp->right;
+                } else { // then temp = root
+                    return true;
+                }
+
+            }
+            return false;
+        }
+};
+
 void BFS() {
     queue<Node*> myQueue;
     myQueue.push(root);
