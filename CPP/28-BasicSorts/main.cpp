5ef971fa8c76b860c7ec68b71cd845e7f3e173f9
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-28 14:40:11 -0700

    main

--- CPP/28-BasicSorts/main.cpp
+++ CPP/28-BasicSorts/main.cpp
@@ -2,10 +2,23 @@
 using namespace std;
 
 
+void bubbleSort(int array[], int size) {
+    for (int i = size - 1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (array[j] > array[j+1]) {
+                int temp = array[j+1];
+                array[j] = array[i];
+                array[j+1] = temp;
+            }
+        }
+    }
+}
 
 int main() {
 
+    int myArray[] = {6,4,2,5,1,3};
+
+    int size = sizeof(myArray) / sizeof(myArray[0]);
     return 0;
 }
-
 
