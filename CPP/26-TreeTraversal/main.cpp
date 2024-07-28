1ea7e11e29ca96ae1d0fb8fc0e4dac3e1c462112
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-27 17:40:11 -0700

    queue tag

--- CPP/26-TreeTraversal/main.cpp
+++ CPP/26-TreeTraversal/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 
+void BFS() {
+    queue<Node*> myQueue;
+    myQueue.push(root);
+    while (myQeuue.size() > 0) {
 
+        Node* currentNode = myQueue.front();
+        myQueue.pop();
+        cout << currentNode->value << " ";
+        if (currentNode->left != nullptr) {
+            myQueue.push(currentNode->left);
+        }
+        if (currentNode->right != nullptr) {
+            myQueue.push(currentNode->right);
+        }
+    }
+}
 
 
 
