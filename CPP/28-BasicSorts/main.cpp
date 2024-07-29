4be20cb8b4ce71bbc349eff74b8e033b756a3108
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-29 11:14:04 -0700

    selectionSort

--- CPP/28-BasicSorts/main.cpp
+++ CPP/28-BasicSorts/main.cpp
@@ -17,7 +17,19 @@ void bubbleSort(int array[], int size) {
 }
 
 void selectionSort(int array[], int size) {
-    
+    for (int i = 0; i < size; i++) {
+        int minIndex = i;
+        for (int j = i+1; j < size; j++) {
+            if (array[j] < array[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (i != minIndex) {
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+    }
 }
 
 int main() {
