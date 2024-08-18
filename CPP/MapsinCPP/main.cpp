59f6d060a402189ca13de7516f7d05cf59d0ffb8
Thomas Yemane <thomasyemane04@gmail.com>
2024-08-18 10:55:36 -0700

    map features

---
+++ CPP/MapsinCPP/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    map<string, int> map;
+
+    map["one"] = 1;
+    map["two"] = 2;
+    map["three"] = 3;
+
+    cout << "Size of map: " << map.size() << endl;
+
+    return 0;
+}
