96e6586a5a677d510373262f77236b81a7d69dfc
Thomas Yemane <thomasyemane04@gmail.com>
2024-08-22 09:54:21 -0700

    binarySearch

--- CPP/BinarySearch/main.cpp
+++ CPP/BinarySearch/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int binarySearch(int arr[], int low, int high, int x) {
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        // check if x is at present at mid 
+        if (arr[mid] == x) {
+            return mid;
+        }
+
+        // if x is greater, then ignore the left half
+        if (arr[mid] < x) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+
+
+
+
 int main() {
 
 
